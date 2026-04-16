9f5f5052f0c26e655e449d95e7f92348e982be1c
alex <alexandermorrison16@gmail.com>
2026-04-16 19:22:11 +1000

    Initial Commit

---
+++ tickr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tickr
+//
+//  Created by Alex Morrison on 16/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tickr/tickrApp.swift
@@ -0,0 +1,17 @@
+//
+//  tickrApp.swift
+//  tickr
+//
+//  Created by Alex Morrison on 16/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct tickrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
